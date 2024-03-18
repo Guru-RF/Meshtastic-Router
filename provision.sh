7cdbda2f7b477fc50b4a4bc4dd4a1aea905df9f9
Joeri van Dooren <joeri@weepee.org>
2024-03-19 00:44:02 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -11,6 +11,6 @@ meshtastic --port ${SERIAL} --set position.fixed_position true --setlat 50.94749
 meshtastic --port ${SERIAL} --set lora.region EU_868
 meshtastic --port ${SERIAL} --set lora.override_duty_cycle true
 meshtastic --port ${SERIAL} --set lora.sx126x_rx_boosted_gain True
-meshtastic --port ${SERIAL} --set lora.tx_power 27
+meshtastic --port ${SERIAL} --set lora.tx_power 30
 meshtastic --port ${SERIAL} --set lora.tx_enabled true
 meshtastic --port ${SERIAL} --set lora.modem_preset LONG_SLOW
