c8b1119f37a0d8a53ca1c3c790541ac157aae6d0
Joeri van Dooren <joeri@weepee.org>
2024-03-18 15:31:31 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -6,19 +6,12 @@ SERIAL="/dev/cu.usbmodem83201"
 
 meshtastic --port ${SERIAL} --set-owner ON3URE
 meshtastic --port ${SERIAL} --set-owner-short URE
-#meshtastic --port ${SERIAL} --ch-set psk default --ch-index 0
-#meshtastic --port ${SERIAL} --ch-set name "ON4AOW" --ch-set psk default --ch-set uplink_enabled false --ch-index 1
 meshtastic --port ${SERIAL} --set device.role ROUTER
 meshtastic --port ${SERIAL} --set position.fixed_position true --setlat 50.9474907 --setlon 4.2401758 --setalt 46
-
-# default
-meshtastic --port ${SERIAL} --ch-longfast
-
-# test manu
-meshtastic --port ${SERIAL} --ch-longslow
-
 meshtastic --port ${SERIAL} --set lora.region EU_868
-meshtastic --port ${SERIAL} --set lora.override_duty_cycle True
+meshtastic --port ${SERIAL} --set lora.override_duty_cycle true
 meshtastic --port ${SERIAL} --set lora.sx126x_rx_boosted_gain True
-meshtastic --port ${SERIAL} --set lora.tx_power 33
-meshtastic --port ${SERIAL} --set lora.tx_enabled True
+meshtastic --port ${SERIAL} --set lora.tx_power 0
+meshtastic --port ${SERIAL} --set lora.tx_enabled true
+meshtastic --port ${SERIAL} --set power.is_power_saving false
+meshtastic --port /dev/cu.usbmodem83101  --ch-set name "SvxLink" --ch-set psk default --ch-index 4
