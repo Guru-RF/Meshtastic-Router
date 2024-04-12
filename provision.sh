72d4b878d46fe48126ac57d29b6d7b8665ac7547
Joeri van Dooren <joeri@weepee.org>
2024-04-12 16:12:23 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -4,13 +4,15 @@ SERIAL="/dev/cu.usbmodem83101"
 
 
 
-meshtastic --port ${SERIAL} --set-owner ON3URE
+meshtastic --port ${SERIAL} --set-owner ON6URE
 meshtastic --port ${SERIAL} --set-owner-short URE
 meshtastic --port ${SERIAL} --set device.role ROUTER
 meshtastic --port ${SERIAL} --set position.fixed_position true --setlat 50.9474907 --setlon 4.2401758 --setalt 46
-meshtastic --port ${SERIAL} --set lora.region EU_868
+meshtastic --port ${SERIAL} --set lora.region EU_433
+meshtastic --port ${SERIAL} --set-ham ON6URE
 meshtastic --port ${SERIAL} --set lora.override_duty_cycle true
 meshtastic --port ${SERIAL} --set lora.sx126x_rx_boosted_gain True
+meshtastic --port ${SERIAL} --ch-set psk none --ch-index 0
 meshtastic --port ${SERIAL} --set lora.tx_power 30
 meshtastic --port ${SERIAL} --set lora.tx_enabled true
 meshtastic --port ${SERIAL} --set lora.modem_preset LONG_SLOW
